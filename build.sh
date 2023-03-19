ecb71f034315eaccbf5facd3f6c1a2d8dff73c29
CreepyCre <CreepyCre@gmail.com>
2023-03-19 16:32:11 +0100

    Deployed 831438a with MkDocs version: 1.4.2

--- build.sh
+++
@@ -1,5 +0,0 @@
-if [ -z "${PYTHONPATH-}" ]
-then export PYTHONPATH=./python
-else PYTHONPATH=./python\;$PYTHONPATH
-fi
-mkdocs build
--- serve.sh
+++
@@ -1,6 +0,0 @@
-if [ -z "${PYTHONPATH-}" ]
-then export PYTHONPATH=./python
-else PYTHONPATH=./python\;$PYTHONPATH
-fi
-echo $PYTHONPATH
-mkdocs serve
